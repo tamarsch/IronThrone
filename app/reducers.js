11b091f2f1ff3a3448c5c6aad640a27b9e312c81
tamar schachter <tam19sh991@gmail.com>
2017-11-08 14:55:52 +0200

    fix issues

--- app/reducers.js
+++ app/reducers.js
@@ -7,7 +7,7 @@ import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
-// import userInputReducer from 'containers/HomePage/reducer';
+import userInputReducer from 'containers/HomePage/reducer';
 
 /*
  * routeReducer
@@ -44,7 +44,7 @@ export default function createReducer(injectedReducers) {
   return combineReducers({
     route: routeReducer,
     language: languageProviderReducer,
-    // userInput: userInputReducer,
+    userInput: userInputReducer,
     ...injectedReducers,
   });
 }
